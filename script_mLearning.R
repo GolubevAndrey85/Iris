41f1e97529146ee9432deb683674725395f8ed0f
Andrew Golubev <GolubevAndrey85@gmail.com>
2017-11-21 21:50:15 +1100

    Correlation added

--- script_mLearning.R
+++ script_mLearning.R
@@ -6,3 +6,23 @@ head(iris)
 names(iris) <- c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species")
 # Check the result
 iris
+
+iris %>% ggvis(~Sepal.Length, ~Sepal.Width, fill = ~Species) %>% layer_points()
+
+# Overall correlation `Petal.Length` and `Petal.Width`
+cor(iris$Petal.Length, iris$Petal.Width)
+
+# Return values of `iris` levels 
+x=levels(iris$Species)
+
+# Print Setosa correlation matrix
+print(x[1])
+cor(iris[iris$Species==x[1],1:4])
+
+# Print Versicolor correlation matrix
+print(x[2])
+cor(iris[iris$Species==x[2],1:4])
+
+# Print Virginica correlation matrix
+print(x[3])
+cor(iris[iris$Species==x[3],1:4])
