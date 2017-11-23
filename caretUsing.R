953b69063b038d9e1204173e5d4681aaa178217e
Andrew Golubev <GolubevAndrey85@gmail.com>
2017-11-23 18:57:34 +1100

    With classified test data

--- caretUsing.R
+++ caretUsing.R
@@ -7,4 +7,23 @@ iris.training <- iris[index,]
 # Subset test set with index
 iris.test <- iris[-index,]
 
+# Train a model
 model_knn <- train(iris.training[, 1:4], iris.training[, 5], method='knn')
+
+# Predict the labels of the test set
+predictions<-predict(object=model_knn,iris.test[,1:4])
+
+# Evaluate the predictions
+table(predictions)
+
+# Confusion matrix 
+confusionMatrix(predictions,iris.test[,5])
+
+# Train the model with preprocessing
+model_knn <- train(iris.training[, 1:4], iris.training[, 5], method='knn', preProcess=c("center", "scale"))
+
+# Predict values
+predictions<-predict.train(object=model_knn,iris.test[,1:4], type="raw")
+
+# Confusion matrix
+confusionMatrix(predictions,iris.test[,5])
